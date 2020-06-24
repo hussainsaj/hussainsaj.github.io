05cb0f9bf37d9fc5b2d7d5160e45fc75806486dd
Hussain Sajid <hussain@Hussains-MacBook-Pro.local>
2020-06-24 10:47:10 +0100

    website structured

--- script/main.js
+++ script/main.js
@@ -10,7 +10,7 @@ class App extends React.Component {
             menu: true
         };
         this.updateProjects = this.updateProjects.bind(this);
-        this.tabs = ['home','blog','projects','bio','contact'] 
+        this.tabs = ['home','blog','projects','about'] 
         this.coverValues = {
             true: {
                 color: 'white',
@@ -28,24 +28,13 @@ class App extends React.Component {
     }
 
     componentDidUpdate() {
-        /*const h1 = document.getElementsByTagName('h1')
-        for (let i=0; i<h1.length; i++) {
-            h1[i].style.color = this.coverValues[this.state.cover].color
-        }
 
-        document.getElementById('background').style.display = this.coverValues[this.state.cover].display
-
-        document.getElementById('background-overlay').style.display = this.coverValues[this.state.cover].display
-
-        if (this.state.tab === 'projects') {
-            this.updateProjects()
-        }*/
     }
 
     render() {
         return (
             <div>
-                <header>
+                {!this.state.cover && <header>
                     <title className='flex'>
                         <div id='logo'>
                             <h1 onClick={() => this.setState({tab: 'home'})}>Hussain Sajid</h1>
@@ -54,7 +43,7 @@ class App extends React.Component {
                             <h1 onClick={() => this.setState({cover: !this.state.cover, rotation: this.state.rotation + 45, tab: 'home'})}>+</h1>
                         </div>
                     </title>
-                    {!this.state.cover && <nav className='flex'>
+                    <nav className='flex'>
                         {this.tabs.map((value, i)=>{
                             const display = this.state.menu ? this.state.tab : value
                             const navLink = <div>
@@ -73,10 +62,10 @@ class App extends React.Component {
                                 }
                             }
                         })}
-                    </nav>}
-                </header>
+                    </nav>
+                </header>}
                 <main>
-                    {this.state.cover && <div>
+                    {/*this.state.cover && <div>
                         <div id='background' className='absolute'>
                             <picture>
                                 <source media="(min-width: 1280px)" srcSet="./media/background-XL.jpeg"></source>
@@ -85,39 +74,37 @@ class App extends React.Component {
                                 <img src='./media/background-S.jpeg' alt='classical background image'></img>
                             </picture>
                         </div>
-                        {/*<div id='background-overlay' className='absolute'>
+                        {<div id='background-overlay' className='absolute'>
                             <div id='background-text'>
                                 <h1>Hussain Sajid</h1>
                                 <h2>I'm a developer</h2>
                             </div>
-                        </div>*/}
-                    </div>}
-                    {/*
+                        </div>}
+                    </div>*/}
                     {this.state.tab === 'home' && <div id='content'>
                         <h2>I help build websites with forward-thinking teams that generate positive and lasting value.</h2>
-                        <h2>(Full site coming soon)</h2>
+                        <p>(Full site coming soon)</p>
                     </div>}
                     {this.state.tab === 'blog' && <div>
-                        
+                        <p>Blog coming soon</p>
+                        <br/>
+                        <h2>All of my blogs</h2>
+                        <p><a className='text' href='https://medium.com/@joyful_inchworm_butterfly_535' rel='noopener' target='_blank'>Medium</a></p>
                     </div>}
                     {this.state.tab === 'projects' && <div>
-                        
+                        <p>Projects coming soon</p>
+                        <br/>
+                        <h2>All of my projects</h2>
+                        <p><a className='text' href='https://github.com/hussainsaj' rel='noopener' target='_blank'>GitHub</a></p>
                     </div>}
-                    {this.state.tab === 'bio' && <div>
+                    {this.state.tab === 'about' && <div>
+                        <h2>About me</h2>
                         <p>I'm an ambitious developer who would like to work as part of a team of like-minded developers in an always challenging environment. With over a year of professional and personal experience, I’m always looking for ways to challenge myself and learn new skills. A strong communicator with the ability to share ideas with the team and client.</p>
+                        <br/>
+                        <h2>Feel free to say hello!</h2>
+                        <p><a className='text' href='https://www.linkedin.com/in/hussainsaj/' rel='noopener' target='_blank'>LinkedIn</a></p>
+                        <p><a className='text' href='mailto:hussain-sajid@outlook.com'>hussain-sajid@outlook.com</a></p>
                     </div>}
-                    {this.state.tab === 'contact' && <div id='contact'>
-                        <div>
-                            <a className='text' href='https://github.com/hussainsaj' rel='noopener' target='_blank'>GitHub</a>
-                        </div>
-                        <div>
-                            <a className='text' href='https://www.linkedin.com/in/hussainsaj/' rel='noopener' target='_blank'>LinkedIn</a>
-                        </div>
-                        <div>
-                            <a className='text' href='mailto:hussain-sajid@outlook.com'>hussain-sajid@outlook.com</a>
-                        </div>
-                    </div>}
-                    */}
                 </main>
             </div>
         )
