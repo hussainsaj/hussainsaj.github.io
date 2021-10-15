579188890467dc7b3d2f51c1018c2b48ef087d79
hussainsaj <hussainsajid@Hussains-MacBook-Air.local>
2021-10-15 13:22:48 +0100

    Added export function

--- test/file.js
+++ test/file.js
@@ -1,15 +1,3 @@
-const variableFunction = (a, b) => {
+export function myFunction (a, b) {
     return a * b
-}
-
-function myFunction (a, b) {
-    return a * b
-}
-
-const objectFunction = {
-    a: 0,
-    b: 0,
-    calculate: function() {
-      return this.a * this.b;
-    }
 }
