2d5c68cd6541d68a3dab3993a2455da6071042e9
Hussain Sajid <hussain-sajid@outlook.com>
2023-06-03 04:16:43 +0200

    updated projects

--- projects.js
+++ projects.js
@@ -6,6 +6,116 @@ function _possibleConstructorReturn(self, call) { if (!self) { throw new Referen
 
 function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
 
+/*class Projects extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            github: {
+                status: false,
+                availability: true,
+                projects: {}
+            }
+        };
+        this.updateProjects = this.updateProjects.bind(this);
+    }
+
+    updateProjects() {
+        fetch("https://api.github.com/users/hussainsaj/repos")
+        .then(res => res.json())
+        .then(
+            (result) => {
+                result.sort((a,b) => {
+                    return new Date(b.updated_at) - new Date(a.updated_at)
+                })
+
+                let projects = {
+                    mockups: [],
+                    udacity: [],
+                    others: []
+                }
+                
+                for (let i=0; i<result.length; i++) {
+                    
+                    result[i].updated_at_formatted = this.props.formatDateTime(result[i].updated_at)
+                    if (result[i].description === null) {
+                        result[i].description = ''
+                    } else if (result[i].description.length > 160) {
+                        result[i].description = result[i].description.slice(0,150) + '...'
+                    }
+                    if(result[i].name.split('-')[0] === 'mockup') {projects['mockups'].push(result[i])}
+                    else if(result[i].name.split('-')[0] === 'udacity') {projects['udacity'].push(result[i])}
+                    else if(result[i].name !== 'hussainsaj.github.io') {projects['others'].push(result[i])}
+                }
+
+                this.setState({github:{status: true, availability: true, author: result[0].owner, projects: projects}})
+            },
+            (error) => {
+                this.setState({github:{status: true, availability: false, projects: {}}})
+                console.error(error)
+            }
+        )
+    }
+
+    componentDidMount() {
+        this.updateProjects()
+    }
+
+    render() {
+        const github = this.state.github.projects
+        return (
+            <div className='content'>
+                {this.state.github.status && this.state.github.availability ? <div>
+                    {Object.keys(github).map(function(key) {
+                        if(github[key].length !== 0) {
+                            return <div className='desktop-flex'>
+                                <h2 className='text'>{key.charAt(0).toUpperCase() + key.slice(1)}</h2>
+                                {github[key].map(project => {
+                                    return <div className='entry text'>
+                                        {key === 'mockups' ?
+                                            <h2><a className='text' href={`/${project.name}`}>{project.name}</a></h2> : 
+                                            <h2>{project.name}</h2>
+                                        }
+                                        <p>{project.description}</p>
+                                        <p>{project.updated_at_formatted}</p>
+                                        <a className='text' href={`https://github.com/hussainsaj/${project.name}`}>View in GitHub</a>
+                                    </div>
+                                })}
+                                <hr/>
+                            </div>
+                        }
+                    })}
+                    <div className='social'>
+                        <h2>More on GitHub</h2>
+                        <div className='entry social_card flex'>
+                            <picture>
+                                <img src={this.state.github.author.avatar_url} alt='GitHub profile icon'></img>
+                            </picture>
+                            <h3><a className='text' href={this.state.github.author.html_url} rel='noopener' target='_blank'>{this.state.github.author.login}</a></h3>
+                        </div>
+                    </div>
+                </div>
+                    : this.state.github.status && !this.state.github.availability ?
+                    <div>
+                    <div className='entry text'>
+                        <p>Error loading results. Please try again later</p>
+                    </div>
+                    <hr/>
+                    <div className='social'>
+                        <h2>All of my projects</h2>
+                        <p><a className='text' href='https://github.com/hussainsaj' rel='noopener' target='_blank'>GitHub</a></p>
+                    </div>
+                </div>
+                    :
+                <div>
+                    <div className='entry text'>
+                        <p>Loading...</p>
+                    </div>
+                </div>}
+            </div>
+        )
+    }
+}*/
+
 var Projects = function (_React$Component) {
     _inherits(Projects, _React$Component);
 
@@ -14,120 +124,114 @@ var Projects = function (_React$Component) {
 
         var _this = _possibleConstructorReturn(this, (Projects.__proto__ || Object.getPrototypeOf(Projects)).call(this, props));
 
-        _this.state = {
-            github: {
-                status: false,
-                availability: true,
-                projects: {}
-            }
-        };
-        _this.updateProjects = _this.updateProjects.bind(_this);
+        _this.categories = [{
+            name: 'Senior digital development analyst (web development)',
+            subtext: "Some of the projects that I've worked on as a Senior digital development analyst (web development).",
+            projects: [{
+                name: 'Personalized User Landing Page',
+                description: 'Created a personalized landing page for logged-in users. The page displays localized information and features a full redesign to enhance user experience.'
+            }, {
+                name: 'Archival Material Ordering System',
+                description: 'Developed an online ordering system for archival research materials. The system includes features for customer inquiries, order management by staff, and interactive communication between users and staff.'
+            }, {
+                name: 'Waste Collection Form Update',
+                description: 'Updated online forms for waste collection services to integrate with new waste management systems. Adapted the forms twice to keep up with changes in the service.'
+            }]
+        }, {
+            name: 'Web developer',
+            subtext: "Some of the projects that I've worked on as a Web developer.",
+            projects: [{
+                name: 'Sports and Events Booking System',
+                description: 'Digitized a manual booking system to streamline the process. The new system features dynamic forms for user inquiries and a management interface for staff to track payments and bookings.'
+            }, {
+                name: 'Highways Report Fault Form Update',
+                description: 'Updated the highways report fault form to display already reported faults on a map, reducing duplicate reports and allowing users to add more information to existing reports.'
+            }]
+        }, {
+            name: 'Personal projects',
+            subtext: "Projects that I've created to solve my real life problems",
+            projects: [{
+                name: 'EyeGlide',
+                description: 'A Chrome extension that implements RSI on the webpage.',
+                githubURL: 'https://github.com/hussainsaj/EyeGlide'
+            }]
+        }, {
+            name: 'Mockups',
+            subtext: "Replica of some of my favourite websites for training purposes",
+            projects: [{
+                name: 'blizzy',
+                description: "Blizzy's mockup website for training purposes",
+                url: 'https://www.hussainsajid.com/mockup-blizzy/',
+                githubURL: 'https://github.com/hussainsaj/mockup-blizzy'
+            }, {
+                name: 'greenspace',
+                description: "The Greenspace's website mockup for training purposes",
+                url: 'https://www.hussainsajid.com/mockup-greenspace/',
+                githubURL: 'https://github.com/hussainsaj/mockup-greenspace'
+            }, {
+                name: 'polestar',
+                description: "Polestar's website mockup",
+                url: 'https://www.hussainsajid.com/mockup-polestar/',
+                githubURL: 'https://github.com/hussainsaj/mockup-polestar'
+            }]
+        }];
         return _this;
     }
 
     _createClass(Projects, [{
-        key: 'updateProjects',
-        value: function updateProjects() {
-            var _this2 = this;
-
-            fetch("https://api.github.com/users/hussainsaj/repos").then(function (res) {
-                return res.json();
-            }).then(function (result) {
-                result.sort(function (a, b) {
-                    return new Date(b.updated_at) - new Date(a.updated_at);
-                });
-
-                var projects = {
-                    mockups: [],
-                    udacity: [],
-                    others: []
-                };
-
-                for (var i = 0; i < result.length; i++) {
-
-                    result[i].updated_at_formatted = _this2.props.formatDateTime(result[i].updated_at);
-                    if (result[i].description === null) {
-                        result[i].description = '';
-                    } else if (result[i].description.length > 160) {
-                        result[i].description = result[i].description.slice(0, 150) + '...';
-                    }
-                    if (result[i].name.split('-')[0] === 'mockup') {
-                        projects['mockups'].push(result[i]);
-                    } else if (result[i].name.split('-')[0] === 'udacity') {
-                        projects['udacity'].push(result[i]);
-                    } else if (result[i].name !== 'hussainsaj.github.io') {
-                        projects['others'].push(result[i]);
-                    }
-                }
-
-                _this2.setState({ github: { status: true, availability: true, author: result[0].owner, projects: projects } });
-            }, function (error) {
-                _this2.setState({ github: { status: true, availability: false, projects: {} } });
-                console.error(error);
-            });
-        }
-    }, {
-        key: 'componentDidMount',
-        value: function componentDidMount() {
-            this.updateProjects();
-        }
-    }, {
         key: 'render',
         value: function render() {
-            var github = this.state.github.projects;
             return React.createElement(
                 'div',
                 { className: 'content' },
-                this.state.github.status && this.state.github.availability ? React.createElement(
+                React.createElement(
                     'div',
                     null,
-                    Object.keys(github).map(function (key) {
-                        if (github[key].length !== 0) {
-                            return React.createElement(
-                                'div',
-                                { className: 'desktop-flex' },
-                                React.createElement(
-                                    'h2',
-                                    { className: 'text' },
-                                    key.charAt(0).toUpperCase() + key.slice(1)
-                                ),
-                                github[key].map(function (project) {
-                                    return React.createElement(
-                                        'div',
-                                        { className: 'entry text' },
-                                        key === 'mockups' ? React.createElement(
-                                            'h2',
-                                            null,
-                                            React.createElement(
-                                                'a',
-                                                { className: 'text', href: '/' + project.name },
-                                                project.name
-                                            )
-                                        ) : React.createElement(
-                                            'h2',
-                                            null,
-                                            project.name
-                                        ),
-                                        React.createElement(
-                                            'p',
-                                            null,
-                                            project.description
-                                        ),
-                                        React.createElement(
-                                            'p',
-                                            null,
-                                            project.updated_at_formatted
-                                        ),
+                    this.categories.map(function (category) {
+                        return React.createElement(
+                            'div',
+                            { className: 'desktop-flex' },
+                            React.createElement(
+                                'h2',
+                                { className: 'text' },
+                                category.name
+                            ),
+                            React.createElement(
+                                'p',
+                                { 'class': 'project-subtext' },
+                                category.subtext
+                            ),
+                            category.projects.map(function (project) {
+                                return React.createElement(
+                                    'div',
+                                    { className: 'entry text' },
+                                    'url' in project ? React.createElement(
+                                        'h2',
+                                        null,
                                         React.createElement(
                                             'a',
-                                            { className: 'text', href: 'https://github.com/hussainsaj/' + project.name },
-                                            'View in GitHub'
+                                            { className: 'text', href: '' + project.url },
+                                            project.name
                                         )
-                                    );
-                                }),
-                                React.createElement('hr', null)
-                            );
-                        }
+                                    ) : React.createElement(
+                                        'h2',
+                                        null,
+                                        project.name
+                                    ),
+                                    'description' in project ? React.createElement(
+                                        'p',
+                                        null,
+                                        project.description
+                                    ) : '',
+                                    'githubURL' in project ? React.createElement(
+                                        'a',
+                                        { className: 'text', href: '' + project.githubURL },
+                                        'View in GitHub'
+                                    ) : ''
+                                );
+                            }),
+                            React.createElement('hr', null)
+                        );
                     }),
                     React.createElement(
                         'div',
@@ -143,60 +247,17 @@ var Projects = function (_React$Component) {
                             React.createElement(
                                 'picture',
                                 null,
-                                React.createElement('img', { src: this.state.github.author.avatar_url, alt: 'GitHub profile icon' })
+                                React.createElement('img', { src: 'https://avatars.githubusercontent.com/u/38162048?v=4', alt: 'GitHub profile icon' })
                             ),
                             React.createElement(
                                 'h3',
                                 null,
                                 React.createElement(
                                     'a',
-                                    { className: 'text', href: this.state.github.author.html_url, rel: 'noopener', target: '_blank' },
-                                    this.state.github.author.login
+                                    { className: 'text', href: 'https://github.com/hussainsaj?tab=repositories', rel: 'noopener', target: '_blank' },
+                                    'hussainsaj'
                                 )
                             )
-                        )
-                    )
-                ) : this.state.github.status && !this.state.github.availability ? React.createElement(
-                    'div',
-                    null,
-                    React.createElement(
-                        'div',
-                        { className: 'entry text' },
-                        React.createElement(
-                            'p',
-                            null,
-                            'Error loading results. Please try again later'
-                        )
-                    ),
-                    React.createElement('hr', null),
-                    React.createElement(
-                        'div',
-                        { className: 'social' },
-                        React.createElement(
-                            'h2',
-                            null,
-                            'All of my projects'
-                        ),
-                        React.createElement(
-                            'p',
-                            null,
-                            React.createElement(
-                                'a',
-                                { className: 'text', href: 'https://github.com/hussainsaj', rel: 'noopener', target: '_blank' },
-                                'GitHub'
-                            )
-                        )
-                    )
-                ) : React.createElement(
-                    'div',
-                    null,
-                    React.createElement(
-                        'div',
-                        { className: 'entry text' },
-                        React.createElement(
-                            'p',
-                            null,
-                            'Loading...'
                         )
                     )
                 )
--- script/projects.js
+++ script/projects.js
@@ -1,4 +1,4 @@
-class Projects extends React.Component {
+/*class Projects extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
@@ -103,6 +103,123 @@ class Projects extends React.Component {
                         <p>Loading...</p>
                     </div>
                 </div>}
+            </div>
+        )
+    }
+}*/
+
+class Projects extends React.Component {
+    constructor(props) {
+        super(props);
+        this.categories = [
+            {
+                name : 'Senior digital development analyst (web development)',
+                subtext: "Some of the projects that I've worked on as a Senior digital development analyst (web development).",
+                projects:  [
+                    {
+                        name: 'Personalized User Landing Page',
+                        description: 'Created a personalized landing page for logged-in users. The page displays localized information and features a full redesign to enhance user experience.'
+                    },
+                    {
+                        name: 'Archival Material Ordering System',
+                        description: 'Developed an online ordering system for archival research materials. The system includes features for customer inquiries, order management by staff, and interactive communication between users and staff.'
+                    },
+                    {
+                        name: 'Waste Collection Form Update',
+                        description: 'Updated online forms for waste collection services to integrate with new waste management systems. Adapted the forms twice to keep up with changes in the service.'
+                    }
+                ]
+            },
+            {
+                name : 'Web developer',
+                subtext: "Some of the projects that I've worked on as a Web developer.",
+                projects:  [
+                    {
+                        name: 'Sports and Events Booking System',
+                        description: 'Digitized a manual booking system to streamline the process. The new system features dynamic forms for user inquiries and a management interface for staff to track payments and bookings.'
+                    },
+                    {
+                        name: 'Highways Report Fault Form Update',
+                        description: 'Updated the highways report fault form to display already reported faults on a map, reducing duplicate reports and allowing users to add more information to existing reports.'
+                    }
+                ]
+            },
+            {
+                name : 'Personal projects',
+                subtext: "Projects that I've created to solve my real life problems",
+                projects:  [
+                    {
+                        name: 'EyeGlide',
+                        description: 'A Chrome extension that implements RSI on the webpage.',
+                        githubURL: 'https://github.com/hussainsaj/EyeGlide'
+                    }
+                ]
+            },
+            {
+                name : 'Mockups',
+                subtext: "Replica of some of my favourite websites for training purposes",
+                projects:  [
+                    {
+                        name: 'blizzy',
+                        description: "Blizzy's mockup website for training purposes",
+                        url: 'https://www.hussainsajid.com/mockup-blizzy/',
+                        githubURL: 'https://github.com/hussainsaj/mockup-blizzy'
+                    },
+                    {
+                        name: 'greenspace',
+                        description: "The Greenspace's website mockup for training purposes",
+                        url: 'https://www.hussainsajid.com/mockup-greenspace/',
+                        githubURL: 'https://github.com/hussainsaj/mockup-greenspace'
+                    },
+                    {
+                        name: 'polestar',
+                        description: "Polestar's website mockup",
+                        url: 'https://www.hussainsajid.com/mockup-polestar/',
+                        githubURL: 'https://github.com/hussainsaj/mockup-polestar'
+                    }
+                ]
+            }
+        ];
+    }
+
+    render() {
+        return (
+            <div className='content'>
+                <div>
+                    {this.categories.map(function(category) {
+                        return <div className='desktop-flex'>
+                            <h2 className='text'>{category.name}</h2>
+                            {/*<p>{category.subtext}</p>*/}
+                            <p class='project-subtext'>{category.subtext}</p>
+                            {category.projects.map(project => {
+                                return <div className='entry text'>
+                                    {'url' in project ?
+                                        <h2><a className='text' href={`${project.url}`}>{project.name}</a></h2> : 
+                                        <h2>{project.name}</h2>
+                                    }
+                                    {'description' in project ?
+                                        <p>{project.description}</p> : 
+                                        ''
+                                    }
+                                    {'githubURL' in project ?
+                                        <a className='text' href={`${project.githubURL}`}>View in GitHub</a> : 
+                                        ''
+                                    }
+                                </div>
+                            })}
+                            <hr/>
+                        </div>
+                    })}
+                    <div className='social'>
+                        <h2>More on GitHub</h2>
+                        <div className='entry social_card flex'>
+                            <picture>
+                                <img src='https://avatars.githubusercontent.com/u/38162048?v=4' alt='GitHub profile icon'></img>
+                            </picture>
+                            <h3><a className='text' href='https://github.com/hussainsaj?tab=repositories' rel='noopener' target='_blank'>hussainsaj</a></h3>
+                        </div>
+                    </div>
+                </div>
             </div>
         )
     }
